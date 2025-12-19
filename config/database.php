aad57ed06334969f06987504a482d4332c2d4597
deniskeller <dentean@mail.ru>
2025-12-19 23:29:41 +0400

    feat: add database config

--- config/database.php
+++ config/database.php
@@ -1,4 +1,19 @@
 <?php
+
 return [
     'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'database'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'options' => [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ],
+        ],
+    ]
 ];
--- core/router.php
+++ core/router.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once CONFIG . '/routes.php';
 //class Router
 //{
 //}
